dbf1fd1cd53319a323253691c7c69addbb724dbd
Michael Venema <michael.venema2010@gmail.com>
2020-08-11 14:03:27 -0700

    Update Pdf_to_jpg.py

--- Pdf_to_jpg.py
+++ Pdf_to_jpg.py
@@ -1,3 +1,6 @@
+#you may need to run "pip3 install pdf2jpg"
+#you also need to have java installed
+
 from pdf2jpg import pdf2jpg
 inputfile = r"<path to your pdf>"
 outputpath = r"<path to your output dir>"
